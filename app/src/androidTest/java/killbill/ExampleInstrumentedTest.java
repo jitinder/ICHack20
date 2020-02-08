ba0af5466afa75596ed2efb72ec7c1f55f196e79
Brython Caley-Davies <bc2918@imperial.ac.uk>
2020-02-08 15:02:12 +0000

    Adding correct package structure

--- app/src/androidTest/java/killbill/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/killbill/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.killbill;
+package killbill;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.killbill", appContext.getPackageName());
+        assertEquals("killbill", appContext.getPackageName());
     }
 }
