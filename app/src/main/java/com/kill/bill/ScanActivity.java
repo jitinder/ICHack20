3c5ac4c5f75d3bebed9d9ea35922431e87d0e3c8
Brython Caley-Davies <bc2918@imperial.ac.uk>
2020-02-08 16:50:52 +0000

    Added image scanning and converting with TODO for function call for image processing

--- app/src/main/java/com/kill/bill/ScanActivity.java
+++ app/src/main/java/com/kill/bill/ScanActivity.java
@@ -1,15 +1,22 @@
 package com.kill.bill;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.Blob;
+
+import java.nio.ByteBuffer;
+
 
 public class ScanActivity extends AppCompatActivity {
+  /* Magic number for camera intent */
   private static final int REQUEST_IMAGE_CAPTURE = 1;
 
   @Override
@@ -19,6 +26,7 @@ public class ScanActivity extends AppCompatActivity {
   }
 
   public void onClick(View view) {
+    /* Switches to Android camera */
     Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
     if (takePicture.resolveActivity(getPackageManager()) != null) {
@@ -28,9 +36,33 @@ public class ScanActivity extends AppCompatActivity {
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    /* If scanning image */
     if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+      /* Gets bitmap from intent */
       Bundle extras = data != null ? data.getExtras() : null;
-      Bitmap image = (Bitmap) extras.get("data");
+      Bitmap image = (Bitmap) (extras != null ? extras.get("data") : null);
+
+      /* Display bitmap onscreen */
+      ((ImageView) findViewById(R.id.image)).setImageBitmap(image);
+
+      if (image != null) {
+        /* Buffer for storing bytes from bitmap*/
+        int size = image.getRowBytes() * image.getHeight();
+        ByteBuffer buffer = ByteBuffer.allocate(size);
+        byte[] array;
+        Blob blob;
+
+        /* Builds Firebase Blob from byte[] */
+        image.copyPixelsToBuffer(buffer);
+        array = buffer.array();
+        blob = Blob.fromBytes(array);
+
+        System.out.println("Success converting image");
+
+        // TODO: function call with blob
+      }
     }
   }
 }
