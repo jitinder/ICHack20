4214371f245b805602c782156baa930726aca1a8
Brython Caley-Davies <bc2918@imperial.ac.uk>
2020-02-08 23:55:04 +0000

    Trying to fix firebase

--- app/src/main/java/com/kill/bill/TestAzureActivity.java
+++ app/src/main/java/com/kill/bill/TestAzureActivity.java
@@ -3,22 +3,27 @@ package com.kill.bill;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.nio.ByteBuffer;
-import java.util.concurrent.ExecutionException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class TestAzureActivity extends AppCompatActivity {
+
+  private static final String baseUrl = "gs://killbill-8ef96.appspot.com/scanned_images";
 
   private static final String subscriptionKey = System.getenv("COMPUTER_VISION_SUBSCRIPTION_KEY");
   private static final String endpoint = ("COMPUTER_VISION_ENDPOINT");
@@ -37,79 +42,33 @@ public class TestAzureActivity extends AppCompatActivity {
     setContentView(R.layout.activity_test_azure);
 
     /*Retrofit retrofit =
-        new Retrofit.Builder()
-            .baseUrl("https://api.imgur.com/3/image")
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build();*/
+    new Retrofit.Builder()
+        .baseUrl("https://api.imgur.com/3/image")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build();*/
 
     Button takeImageButton = findViewById(R.id.takePictureButton);
     takeImageButton.setOnClickListener(
         view -> {
           StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
           StrictMode.setVmPolicy(builder.build());
-          takePhoto();
-          galleryAddPic();
+          imageUri = takePhoto();
         });
   }
 
-  public void takePhoto() {
+  public Uri takePhoto() {
     Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-    //File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
+    Uri uri = Uri.fromFile(new File(getFilesDir() + "/pic.jpg"));
 
-    //imageUri = Uri.fromFile(photo);
-    //intent.putExtra(MediaStore.EXTRA_OUTPUT, this.imageUri);
+    intent.putExtra(MediaStore.EXTRA_OUTPUT, this.imageUri);
 
     this.startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+
+    return uri;
   }
 
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-
-    /* If scanning image */
-    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-      /* Gets bitmap from intent */
-      Bundle extras = data != null ? data.getExtras() : null;
-      Bitmap image = (Bitmap) (extras != null ? extras.get("data") : null);
-
-      /* Display bitmap onscreen */
-      //((ImageView) findViewById(R.id.image)).setImageBitmap(image);
-
-      if (image != null) {
-        /* Buffer for storing bytes from bitmap*/
-        int size = image.getRowBytes() * image.getHeight();
-        ByteBuffer buffer = ByteBuffer.allocate(size);
-        byte[] array;
-
-        /* Builds Firebase Blob from byte[] */
-        image.copyPixelsToBuffer(buffer);
-        array = buffer.array();
-
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageReference = storage.getReference();
-        StorageReference imageRef = storageReference.child("scanned_images/pic.jpg");
-
-        try {
-          AsyncTask.execute(() -> {
-            try {
-              Tasks.await(imageRef.putBytes(array));
-            } catch (ExecutionException | InterruptedException e) {
-              e.printStackTrace();
-            }
-          });
-
-          Uri downloadUri = imageRef.getDownloadUrl().getResult();
-          System.out.println(downloadUri != null ? downloadUri.toString() : null);
-        } catch (Exception e) {
-          System.out.println("This is fun");
-          e.printStackTrace();
-        }
-      }
-    }
-  }
-
-  private void galleryAddPic() {
+  /*private void galleryAddPic() {
     Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
     File f = new File(String.valueOf(imageUri));
     Uri contentUri = Uri.fromFile(f);
@@ -117,5 +76,38 @@ public class TestAzureActivity extends AppCompatActivity {
     mediaScanIntent.setData(contentUri);
 
     this.sendBroadcast(mediaScanIntent);
+  }*/
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    Bitmap bm = (Bitmap) data.getExtras().get("data");
+    ((ImageView) findViewById(R.id.image)).setImageBitmap(bm);
+
+    File file = new File(getFilesDir() + "/pic.jpg");
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageReference = storage.getReference();
+    StorageReference imageRef = storageReference.child("scanned/pic.jpg");
+
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+    byte[] bytes = stream.toByteArray();
+
+    try {
+      FileOutputStream fos = new FileOutputStream(file);
+      fos.write(bytes);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+    UploadTask task = imageRef.putFile(Uri.fromFile(file));
+    task.pause();
+    task.resume();
+
+    task.addOnFailureListener(o -> System.out.println("Failure"))
+        .addOnSuccessListener(o -> System.out.println("Success"));
   }
 }
