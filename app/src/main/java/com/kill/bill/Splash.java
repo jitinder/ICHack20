6597888a2427ccfc72edb408a31fd755f9c0fb0a
CristinaGT <cristina@asus>
2020-02-08 20:33:11 +0000

    loading animation

--- app/src/main/java/com/kill/bill/Splash.java
+++ app/src/main/java/com/kill/bill/Splash.java
@@ -2,13 +2,23 @@ package com.kill.bill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
-
+    AnimationDrawable loadingAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView loading = (ImageView)findViewById(R.id.imageView);
+        loadingAnimation = (AnimationDrawable)loading.getDrawable();
+        loadingAnimation.start();
     }
+
+
+
 }
