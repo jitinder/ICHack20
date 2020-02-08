8cbbab75196d4e0578b55d83e8c7cc478eaf1d93
CristinaGT <cristina@asus>
2020-02-08 22:38:12 +0000

    Loading Screen

--- app/src/main/java/com/kill/bill/Splash.java
+++ app/src/main/java/com/kill/bill/Splash.java
@@ -2,6 +2,7 @@ package com.kill.bill;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +12,15 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        ImageView loading = findViewById(R.id.imageView);
-        loading.setBackgroundResource(R.drawable.loading);
-        loadingAnimation = (AnimationDrawable) loading.getBackground();
+        ImageView loading = findViewById(R.id.imageViewReceipt);
+        loading.setImageResource(R.drawable.loading);
+        //loading.setBackgroundResource(R.drawable.loading);
+        loadingAnimation = (AnimationDrawable) loading.getDrawable();
         loadingAnimation.start();
     }
 
