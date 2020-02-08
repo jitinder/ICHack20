92e50f4aff010f765272ca7ef280cf0d8316871e
ChrisUnderhill <cpu22@cam.ac.uk>
2020-02-08 20:23:53 +0000

    Azure service identification

--- app/src/main/java/com/kill/bill/TestAzureActivity.java
+++ app/src/main/java/com/kill/bill/TestAzureActivity.java
@@ -1,36 +1,30 @@
 package com.kill.bill;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.net.Uri;
+import android.util.JsonReader;
 import android.util.Log;
 
-import java.io.BufferedReader;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 import javax.net.ssl.HttpsURLConnection;
-
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class TestAzureActivity extends AppCompatActivity {
 
-    private static final String subscriptionKey = "87e56323493740c7a179825fa8cfc9ed"; //System.getenv("87e56323493740c7a179825fa8cfc9ed");
+    private static final String subscriptionKey = "87e56323493740c7a179825fa8cfc9ed";// System.getenv("KILL_BILL_SUB_KEY");
     private static final String endpoint = ("https://killbillcomputervision.cognitiveservices.azure.com/");
 
 
-    private static final String uriBase = endpoint +
+    private static final String readURI = endpoint +
             "vision/v2.1/read/core/asyncBatchAnalyze";
 
     private static final String imageToAnalyze =
@@ -41,90 +35,116 @@ public class TestAzureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_azure);
 
-        /*
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(uriBase)
-        .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
+        Log.e("SUBKEY", "" + subscriptionKey);
 
-        */
 
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                // All your networking logic
-                // should be here
-
 
                 try {
-                    URL githubEndpoint = new URL(uriBase);
-                    HttpsURLConnection myConnection =
-                            (HttpsURLConnection) githubEndpoint.openConnection();
+                    URL readURI = new URL(TestAzureActivity.readURI);
+                    HttpsURLConnection readConnection =
+                            (HttpsURLConnection) readURI.openConnection();
 
-                    String myData = "{\"url\":\"https://i.imgur.com/TQXB8ds.jpg\"}";
+                    String myData = "{\"url\":\"" + imageToAnalyze + "\"}";
 
-                    myConnection.setRequestMethod("POST");
-
-                    myConnection.setRequestProperty("Content-Type", "application/json");
-
-                    myConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+                    readConnection.setRequestMethod("POST");
+                    readConnection.setRequestProperty("Content-Type", "application/json");
+                    readConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
 
                     // Enable writing
-                    myConnection.setDoOutput(true);
+                    readConnection.setDoOutput(true);
 
                     // Write the data
-                    myConnection.getOutputStream().write(myData.getBytes());
+                    readConnection.getOutputStream().write(myData.getBytes());
 
+                    String outputEndpoint = "";
 
-                    int response = myConnection.getResponseCode();
+                    int response = readConnection.getResponseCode();
                     if ( response == 200) {
                         Log.d("CHECK", "200");
                     } else if ( response == 202) {
                         Log.d("CHECK", "202");
-                        InputStream responseBody = myConnection.getInputStream();
 
-                        InputStreamReader responseBodyReader =
-                                new InputStreamReader(responseBody, "UTF-8");
+                        outputEndpoint = readConnection.getHeaderField("Operation-Location");
 
-                        Log.d("CHECK", "" + responseBodyReader.read());
-                        myConnection.get
-                        Log.d("CHECK", "" + myConnection.getResponseMessage());
-
-
-
-                        StringBuilder textBuilder = new StringBuilder();
-
-                        try (Reader reader = new BufferedReader(new InputStreamReader(responseBody, Charset.forName(StandardCharsets.UTF_8.name())))){
-                            Log.d("CHECK", "try");
-
-                            int c = 0;
-                            while ((c = reader.read()) != -1) {
-                                Log.d("CHECK", "while");
-                                textBuilder.append((char) c);
-                            }
-                        } catch (Exception e){
-
-                        }
-
-                        Log.d("CHECK", textBuilder.toString());
-                        //InputStreamReader responseBodyReader =
-                        //       new InputStreamReader(responseBody, "UTF-8");
                     } else{
                         // Error handling code goes here
                         Log.e("CHECK", "Failed with " + response);
-                        Log.e("CHECK", "Failed with " + myConnection);
+                        Log.e("CHECK", "Failed with " + readConnection);
                     }
 
+                    Log.d("CHECK", outputEndpoint);
+
+
+                    Thread.sleep(1000);
+                    //We should probably mke this an async promise of some kind that pings the server repeatedly with a nice twirling loading animation
+
+
+                    URL jsonURI = new URL(outputEndpoint);
+                    HttpsURLConnection jsonConnection =
+                            (HttpsURLConnection) jsonURI.openConnection();
+
+                    jsonConnection.setRequestMethod("GET");
+
+                    jsonConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+
+                    response = jsonConnection.getResponseCode();
+                    if ( response == 200) {
+                        Log.d("CHECKOUT", "200");
+                        outputEndpoint = jsonConnection.getHeaderField("Content-Length");
+
+                        InputStream responseBody = jsonConnection.getInputStream();
+                        InputStreamReader responseBodyReader =
+                                new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+
+                        jsonReader.beginObject(); // Start processing the JSON object
+                        while (jsonReader.hasNext()) { // Loop through all keys
+                            String key = jsonReader.nextName(); // Fetch the next key
+                            if (key.equals("status")) {
+                                String value = jsonReader.nextString();
+
+                                Log.i("JSON", value);
+
+                                if (!value.equals("Succeeded")){
+                                    throw new RuntimeException("Failed to parse text from image: status was not Succeeded");
+                                }
+                                break;
+                            } else {
+                                jsonReader.skipValue(); // Skip values of other keys
+                            }
+                        }
+
+                        jsonReader.close();
+                        jsonConnection.disconnect();
+
+                    } else if ( response == 202) {
+                        Log.d("CHECKOUT", "202");
+
+                    } else{
+                        // Error handling code goes here
+                        Log.e("CHECKOUT", "Failed with " + response);
+                        Log.e("CHECKOUT", "Failed with " + jsonConnection);
+                    }
+
+                    Log.d("CHECKOUTLENGTH", outputEndpoint);
+
+
+
                 } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-
 
 
             }
