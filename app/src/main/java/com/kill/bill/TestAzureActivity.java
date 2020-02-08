3537f30abc8f2fbea577f88762db07a1173d4a4c
Sidak <sidak.pasrichassp@gmail.com>
2020-02-08 19:48:48 +0000

    TestAzure

--- app/src/main/java/com/kill/bill/TestAzureActivity.java
+++ app/src/main/java/com/kill/bill/TestAzureActivity.java
@@ -1,12 +1,24 @@
 package com.kill.bill;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import android.content.Intent;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import android.os.Bundle;
+import android.net.Uri;
+
 
 
 public class TestAzureActivity extends AppCompatActivity {
@@ -27,12 +39,44 @@ public class TestAzureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_azure);
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.themoviedb.org/3/")
+                .baseUrl("https://api.imgur.com/3/image")
         .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
+        Button takeImageButton = findViewById(R.id.takePictureButton);
+        takeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                takePhoto();
+                galleryAddPic();
+            }
+        });
+    }
 
+    Uri imageUri;
 
+    public void takePhoto() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        this.startActivityForResult(intent, 100);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(String.valueOf(imageUri));
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
