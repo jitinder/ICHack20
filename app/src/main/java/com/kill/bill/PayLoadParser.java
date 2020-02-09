fe0676671c09838967c6a7ab2326c1bd8ddc63e2
Brython Caley-Davies <bc2918@imperial.ac.uk>
2020-02-09 04:21:16 +0000

    Fixing merge conflicts and adding more json examples

--- app/src/main/java/com/kill/bill/PayLoadParser.java
+++ app/src/main/java/com/kill/bill/PayLoadParser.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PayLoadParser extends AppCompatActivity {
+
+  private final List<Item> items = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +24,20 @@ public class PayLoadParser extends AppCompatActivity {
     setContentView(R.layout.activity_pay_load_parser);
 
     try {
-      parseJSON();
+      parseJSON("example_1.json");
+      parseJSON("example_2.json");
+      parseJSON("example_3.json");
     } catch (Exception e) {
       System.out.println("JSON Exception");
     }
   }
 
-  private void parseJSON() {
+  private void parseJSON(String filename) {
     JsonObject root;
     JsonParser parser = new JsonParser();
 
     try {
-      root = parser.parse(new FileReader("parser.json")).getAsJsonObject();
+      root = parser.parse(new FileReader(filename)).getAsJsonObject();
     } catch (FileNotFoundException | ClassCastException e) {
       e.printStackTrace();
       return;
@@ -43,9 +50,13 @@ public class PayLoadParser extends AppCompatActivity {
       JsonArray lines = pageLines.getAsJsonArray("lines");
 
       for (int line = 0; line < lines.size(); line++) {
-        JsonObject lineData = lines.get(line).getAsJsonObject();
+        Item item = new Item();
+        JsonElement lineData = lines.get(line).getAsJsonObject().get("text");
 
-        System.out.println(lineData.get("text"));
+        System.out.println(lineData);
+
+        item.setName(lineData.toString());
+        this.items.add(item);
       }
     }
   }
