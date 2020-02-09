95410b721e1cfa7383941109d6ef47aeeabf964c
Sidak <sidak.pasrichassp@gmail.com>
2020-02-09 01:34:53 +0000

    Changed Azure work to AsyncTask

---
+++ app/src/main/java/com/kill/bill/Item.java
@@ -0,0 +1,31 @@
+package com.kill.bill;
+
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
--- app/src/main/java/com/kill/bill/TestAzureActivity.java
+++ app/src/main/java/com/kill/bill/TestAzureActivity.java
@@ -1,12 +1,16 @@
 package com.kill.bill;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.JsonReader;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -25,6 +29,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
+import java.util.concurrent.ExecutionException;
+
 import android.os.Bundle;
 import android.net.Uri;
 
@@ -50,124 +56,7 @@ public class TestAzureActivity extends AppCompatActivity {
 
         Log.e("SUBKEY", "" + subscriptionKey);
 
-
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    URL readURI = new URL(TestAzureActivity.readURI);
-                    HttpsURLConnection readConnection =
-                            (HttpsURLConnection) readURI.openConnection();
-
-                    String myData = "{\"url\":\"" + imageToAnalyze + "\"}";
-
-                    readConnection.setRequestMethod("POST");
-                    readConnection.setRequestProperty("Content-Type", "application/json");
-                    readConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
-
-                    // Enable writing
-                    readConnection.setDoOutput(true);
-
-                    // Write the data
-                    readConnection.getOutputStream().write(myData.getBytes());
-
-                    String outputEndpoint = "";
-
-                    int response = readConnection.getResponseCode();
-                    if ( response == 200) {
-                        Log.d("CHECK", "200");
-                    } else if ( response == 202) {
-                        Log.d("CHECK", "202");
-
-                        outputEndpoint = readConnection.getHeaderField("Operation-Location");
-
-                    } else{
-                        // Error handling code goes here
-                        Log.e("CHECK", "Failed with " + response);
-                        Log.e("CHECK", "Failed with " + readConnection);
-                    }
-
-                    Log.d("CHECK", outputEndpoint);
-
-
-                    Thread.sleep(1000);
-                    //We should probably mke this an async promise of some kind that pings the server repeatedly with a nice twirling loading animation
-
-
-                    URL jsonURI = new URL(outputEndpoint);
-                    HttpsURLConnection jsonConnection =
-                            (HttpsURLConnection) jsonURI.openConnection();
-
-                    jsonConnection.setRequestMethod("GET");
-
-                    jsonConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
-
-
-                    response = jsonConnection.getResponseCode();
-                    if ( response == 200) {
-                        Log.d("CHECKOUT", "200");
-                        outputEndpoint = jsonConnection.getHeaderField("Content-Length");
-
-                        InputStream responseBody = jsonConnection.getInputStream();
-                        InputStreamReader responseBodyReader =
-                                new InputStreamReader(responseBody, "UTF-8");
-                        JsonReader jsonReader = new JsonReader(responseBodyReader);
-
-                        jsonReader.beginObject(); // Start processing the JSON object
-                        while (jsonReader.hasNext()) { // Loop through all keys
-                            String key = jsonReader.nextName(); // Fetch the next key
-                            if (key.equals("status")) {
-                                String value = jsonReader.nextString();
-
-                                Log.i("JSON", value);
-
-                                if (!value.equals("Succeeded")){
-                                    throw new RuntimeException("Failed to parse text from image: status was not Succeeded");
-                                }
-                                break;
-                            } else {
-                                jsonReader.skipValue(); // Skip values of other keys
-                            }
-                        }
-
-                        jsonReader.close();
-                        jsonConnection.disconnect();
-
-                    } else if ( response == 202) {
-                        Log.d("CHECKOUT", "202");
-
-                    } else{
-                        // Error handling code goes here
-                        Log.e("CHECKOUT", "Failed with " + response);
-                        Log.e("CHECKOUT", "Failed with " + jsonConnection);
-                    }
-
-                    Log.d("CHECKOUTLENGTH", outputEndpoint);
-
-
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-        });
-      
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.imgur.com/3/image")
-        .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
+        new GetImageText(this).execute(readURI, null, "");
 
         Button takeImageButton = findViewById(R.id.takePictureButton);
         takeImageButton.setOnClickListener(new View.OnClickListener() {
@@ -177,6 +66,15 @@ public class TestAzureActivity extends AppCompatActivity {
                 StrictMode.setVmPolicy(builder.build());
                 takePhoto();
                 galleryAddPic();
+            }
+        });
+
+        Button goToPayload = findViewById(R.id.gottopayload);
+        goToPayload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), PayLoadParser.class);
+                startActivity(intent);
             }
         });
     }
@@ -203,5 +101,112 @@ public class TestAzureActivity extends AppCompatActivity {
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
+    }
+
+    private class GetImageText extends AsyncTask<String, Void, String>{
+        private ProgressDialog dialog;
+
+        public GetImageText(Activity activity) {
+            this.dialog = new ProgressDialog(activity);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                URL readURI = new URL(strings[0]);
+                HttpsURLConnection readConnection =
+                        (HttpsURLConnection) readURI.openConnection();
+
+                String myData = "{\"url\":\"" + imageToAnalyze + "\"}";
+
+                readConnection.setRequestMethod("POST");
+                readConnection.setRequestProperty("Content-Type", "application/json");
+                readConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Enable writing
+                readConnection.setDoOutput(true);
+
+                // Write the data
+                readConnection.getOutputStream().write(myData.getBytes());
+
+                String outputEndpoint = "";
+
+                int response = readConnection.getResponseCode();
+                if (response == 200) {
+                    Log.d("CHECK", "200");
+                } else if (response == 202) {
+                    Log.d("CHECK", "202");
+
+                    outputEndpoint = readConnection.getHeaderField("Operation-Location");
+
+                } else {
+                    // Error handling code goes here
+                    Log.e("CHECK", "Failed with " + response);
+                    Log.e("CHECK", "Failed with " + readConnection);
+                }
+
+                Log.d("CHECK", outputEndpoint);
+
+                URL jsonURI = new URL(outputEndpoint);
+                HttpsURLConnection jsonConnection =
+                        (HttpsURLConnection) jsonURI.openConnection();
+
+                jsonConnection.setRequestMethod("GET");
+
+                jsonConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+
+                response = jsonConnection.getResponseCode();
+                if (response == 200) {
+                    Log.d("CHECKOUT", "200");
+
+                    InputStream responseBody = jsonConnection.getInputStream();
+                    InputStreamReader responseBodyReader =
+                            new InputStreamReader(responseBody, "UTF-8");
+                    BufferedReader br = new BufferedReader(responseBodyReader);
+                    StringBuilder sb = new StringBuilder();
+                    String inputLine;
+                    while ((inputLine = br.readLine()) != null) {
+                        sb.append(inputLine);
+                        System.out.println(inputLine);
+                    }
+                    br.close();
+                    //System.out.println(sb.toString());
+                    jsonConnection.disconnect();
+
+                    return sb.toString();
+
+                } else if (response == 202) {
+                    Log.d("CHECKOUT", "202");
+
+                } else {
+                    // Error handling code goes here
+                    Log.e("CHECKOUT", "Failed with " + response);
+                    Log.e("CHECKOUT", "Failed with " + jsonConnection);
+                }
+
+                Log.d("CHECKOUTLENGTH", outputEndpoint);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return "";
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Analysing your receipt");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            System.out.println("Payload: " +s);
+        }
     }
 }
