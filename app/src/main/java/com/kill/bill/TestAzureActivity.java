4aa754b88583c1d36d31a2af1d51b6bb189a694a
ChrisUnderhill <cpu22@cam.ac.uk>
2020-02-09 01:36:06 +0000

    Backup

--- app/src/main/java/com/kill/bill/TestAzureActivity.java
+++ app/src/main/java/com/kill/bill/TestAzureActivity.java
@@ -1,5 +1,6 @@
 package com.kill.bill;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.JsonReader;
@@ -7,6 +8,7 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -47,6 +49,10 @@ public class TestAzureActivity extends AppCompatActivity {
     private static final String imageToAnalyze =
             "https://i.imgur.com/TQXB8ds.jpg";
 
+
+    private String downloadEndpoint =
+            "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,7 +82,7 @@ public class TestAzureActivity extends AppCompatActivity {
                     // Write the data
                     readConnection.getOutputStream().write(myData.getBytes());
 
-                    String outputEndpoint = "";
+
 
                     int response = readConnection.getResponseCode();
                     if ( response == 200) {
@@ -84,7 +90,7 @@ public class TestAzureActivity extends AppCompatActivity {
                     } else if ( response == 202) {
                         Log.d("CHECK", "202");
 
-                        outputEndpoint = readConnection.getHeaderField("Operation-Location");
+                        downloadEndpoint = readConnection.getHeaderField("Operation-Location");
 
                     } else{
                         // Error handling code goes here
@@ -92,14 +98,16 @@ public class TestAzureActivity extends AppCompatActivity {
                         Log.e("CHECK", "Failed with " + readConnection);
                     }
 
-                    Log.d("CHECK", outputEndpoint);
+                    Log.d("CHECK", downloadEndpoint);
 
 
                     Thread.sleep(1000);
                     //We should probably mke this an async promise of some kind that pings the server repeatedly with a nice twirling loading animation
 
 
-                    URL jsonURI = new URL(outputEndpoint);
+                    //final ProgressDialog progressDialog = new ProgressDialog(TestAzureActivity.this);
+
+                    URL jsonURI = new URL(downloadEndpoint);
                     HttpsURLConnection jsonConnection =
                             (HttpsURLConnection) jsonURI.openConnection();
 
@@ -111,7 +119,7 @@ public class TestAzureActivity extends AppCompatActivity {
                     response = jsonConnection.getResponseCode();
                     if ( response == 200) {
                         Log.d("CHECKOUT", "200");
-                        outputEndpoint = jsonConnection.getHeaderField("Content-Length");
+                        downloadEndpoint = jsonConnection.getHeaderField("Content-Length");
 
                         InputStream responseBody = jsonConnection.getInputStream();
                         InputStreamReader responseBodyReader =
@@ -153,7 +161,7 @@ public class TestAzureActivity extends AppCompatActivity {
                             Log.e("CHECKOUT", "Failed with " + jsonConnection);
                         }
 
-                        Log.d("CHECKOUTLENGTH", outputEndpoint);
+                        Log.d("CHECKOUTLENGTH", downloadEndpoint);
                 } catch (MalformedURLException ex) {
                     ex.printStackTrace();
                 } catch (ProtocolException ex) {
@@ -201,4 +209,147 @@ public class TestAzureActivity extends AppCompatActivity {
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
     }
+
+
+
+//  private class DownloadTask extends AsyncTask<Void, Integer, String> {
+//    @Override
+//    protected String doInBackground(Void...voids) {
+//      String result = "";
+//
+//      BufferedReader reader = null;
+//
+//      publishProgress(0);
+//
+//      try {
+//          URL jsonURI = new URL(downloadEndpoint);
+//          HttpsURLConnection jsonConnection =
+//                  (HttpsURLConnection) jsonURI.openConnection();
+//
+//          jsonConnection.setRequestMethod("GET");
+//
+//          jsonConnection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+//
+//
+//          int response = jsonConnection.getResponseCode();
+//          if ( response == 200) {
+//              Log.d("CHECKOUT", "200");
+//              downloadEndpoint = jsonConnection.getHeaderField("Content-Length");
+//
+//              InputStream responseBody = jsonConnection.getInputStream();
+//              InputStreamReader responseBodyReader =
+//                      new InputStreamReader(responseBody, "UTF-8");
+//
+//              JsonReader jsonReader = new JsonReader(responseBodyReader);
+//
+//              jsonReader.beginObject(); // Start processing the JSON object
+//              while (jsonReader.hasNext()) { // Loop through all keys
+//                  String key = jsonReader.nextName(); // Fetch the next key
+//                  if (key.equals("status")) {
+//                      String value = jsonReader.nextString();
+//
+//                      Log.i("JSON", value);
+//
+//                      if (!value.equals("Succeeded")) {
+//                          throw new RuntimeException("Failed to parse text from image: status was not Succeeded");
+//                      }
+//                  } else if (key.equals("recognitionResults")) {
+//                      jsonReader.beginArray();
+//                      jsonReader.beginObject();
+//
+//                  } else {
+//                      jsonReader.skipValue(); // Skip values of other keys
+//                  }
+//              }
+//
+//
+//              jsonReader.close();
+//              jsonConnection.disconnect();
+//
+//
+//          } else if ( response == 202) {
+//              Log.d("CHECKOUT", "202");
+//
+//          } else{
+//              // Error handling code goes here
+//              Log.e("CHECKOUT", "Failed with " + response);
+//              Log.e("CHECKOUT", "Failed with " + jsonConnection);
+//          }
+//
+//          Log.d("CHECKOUTLENGTH", downloadEndpoint);
+//
+//
+//
+//        //connection.getContentType() should return something like "application/json; charset=utf-8"
+//        String[] values = connection.getContentType().toString().split(";");
+//        String charset = "";
+//        for (String value : values) {
+//          value = value.trim();
+//          if (value.toLowerCase().startsWith("charset=")) {
+//            charset = value.substring("charset=".length());
+//            break;
+//          }
+//        }
+//        //Set default value if charset not set
+//        if(charset.equals("")) charset = "utf-8";
+//
+//        int contentLength = connection.getContentLength();
+//        InputStream stream = connection.getInputStream();
+//        reader  = new BufferedReader(new InputStreamReader(stream));
+//        StringBuilder builder = new StringBuilder();
+//        /**
+//         * connection.getContentLength() can return -1 on some connections.
+//         * If we have the content length calculate progress, else just set progress to 100 and build the string all at once.
+//         *
+//         */
+//        if(contentLength>-1){
+//          //Odd byte array sizes don't always work, tried 512, 1024, 2048; 1024 is the magic number because it seems to work best.
+//          byte[] data = new byte[1024];
+//          int totalRead = 0;
+//          int bytesRead = 0;
+//          while ((bytesRead = stream.read(data)) > 0) {
+//            try {
+//              builder.append(new String(data, 0, bytesRead, charset));
+//            } catch (UnsupportedEncodingException e) {
+//              Log.e(TAG, "Invalid charset: " + e.getMessage());
+//              //Append without charset (uses system's default charset)
+//              builder.append(new String(data, 0, bytesRead));
+//            }
+//            totalRead += bytesRead;
+//            int progress = (int) (totalRead * (100/(double) contentLength));
+//            //Log.d(TAG, "length = " + contentLength + " bytesRead = " + bytesRead + " totalRead = " + totalRead + " progress = " + progress);
+//            publishProgress(progress);
+//          }
+//        } else {
+//          String line = "";
+//          while ((line = reader.readLine()) != null) {
+//            builder.append(line + '\n');
+//            publishProgress(100);
+//          }
+//        }
+//        result = builder.toString();
+//
+//      } catch (Exception e) {
+//        Log.e(TAG, "Exception "+e.toString());
+//      } finally {
+//        if(reader != null){
+//          try {
+//            reader.close();
+//          } catch(IOException ioe) {
+//            ioe.printStackTrace();
+//          }
+//        }
+//      }
+//      return result;
+//    }
+//
+//    protected void onProgressUpdate(Integer... progress) {
+//      if(!this.isCancelled()) command.onProgressUpdate(progress[0]);
+//    }
+//
+//    @Override
+//    protected void onPostExecute(String result){
+//      if(!this.isCancelled()) downloadTaskComplete(result);
+//    }
+//  }
 }
